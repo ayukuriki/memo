311fc01b56acbef20ae1664b22c2ffcf288a0f4a
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-03-26 11:09:46 +0900

    first submit

---
+++ memo.rb
@@ -0,0 +1,38 @@
+require "csv"
+
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+memo_type = gets.to_i
+
+
+if memo_type == 1 then
+    puts "拡張子を除いたファイルを入力してください"
+    text_name = gets
+
+
+    puts "メモしたい内容を記入してください"
+    puts "完了したらCtrl + Dをおします"
+
+    text_contents = gets.to_s
+    
+   CSV.open("#{text_name}.csv",'w') do |csv|
+        csv << [text_contents]
+    end
+
+ elsif memo_type == 2 then
+    puts "拡張子を除いたファイルを入力してください"
+    
+    exist_text_name = gets
+
+    puts "メモしたい内容を記入してください"
+    puts "完了したらCtrl + Dをおします"
+
+    new_text_contents = gets.to_s
+
+    CSV.open("#{exist_text_name}.csv",'a') do |csv|
+        csv << [new_text_contents]
+    end
+
+ else 
+    puts "正常に作動してません" 
+end
+
